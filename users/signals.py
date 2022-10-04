b79fdd3b1f1df6f5eedf65bb10518819df2b2c2a
brs14shn <brsshn292929@gmail.com>
2022-10-04 15:29:21 +0300

    signal

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.models import User
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
