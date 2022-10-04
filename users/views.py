c3205e061a37b3fe4bbc30062567843e923f233a
brs14shn <brsshn292929@gmail.com>
2022-10-04 16:16:44 +0300

    token added

--- users/views.py
+++ users/views.py
@@ -1,12 +1,30 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializers
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.response import Response
+
 
 
 
 class RegisterView(generics.CreateAPIView):
     queryset=User.objects.all()
     serializer_class=RegisterSerializers
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = serializer.data
+        if Token.objects.filter(user=user).exists():
+            token = Token.objects.get(user=user)
+            data["token"] = token.key
+        else:
+            data["token"] = "No token created for this user.... :))"
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 
