38ab4d23378884d8e68a0459259a28c4f3892f09
brs14shn <brsshn292929@gmail.com>
2022-10-05 14:26:54 +0300

    stock app model created

--- main/urls.py
+++ main/urls.py
@@ -24,6 +24,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+   #  path('nested_admin/',include('nested_admin.urls')),
+   
 
    path("swagger(?P<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+import nested_admin
 
-# Register your models here.
+# # Register your models here.
+from .models import (
+    Category,
+    Product,
+    Firm,
+    Brand,
+    Stock
+)
+
+
+
+
+
+
+# class CategoryAdmin(nested_admin.NestedTabularInline):
+#     model=Category
+# class BrandAdmin(nested_admin.NestedTabularInline):
+#     model=Brand
+# class ProductAdmin(nested_admin.NestedModelAdmin):
+#     model=Category
+#     inlines=[CategoryAdmin,BrandAdmin]
+
+
+admin.site.register(Firm)
+admin.site.register(Category)
+admin.site.register(Brand)
+# admin.site.register(Product,ProductAdmin)
+admin.site.register(Product)
+# admin.site.register(Firm)
+admin.site.register(Stock)
